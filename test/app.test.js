14b05a5c7110ebc6f4d90ca39242837da69128a8
Abishekvarshan <varsityabi@gmail.com>
2025-10-26 18:10:57 +0530

    writing test2

--- test/app.test.js
+++ test/app.test.js
@@ -6,7 +6,7 @@ const {app, server, name } = require('../app')
 describe('GET /',()=>{
    it("should return 200 ststusand the correct message",async()=>{
     const response = await request(app).get("/");
-    expect(response.status.toBe(200));
+     expect(response.status).toBe(200);
     expect(response.text).toBe(`Welcome to ShopEasy from${name}!`)
    })
 
